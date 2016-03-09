bb0ec33ff80a88133d7399c4da73605d1a52c99f
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-09 08:21:24 -0800

    Add empty default constructor for SimpleButtonListFragment

--- app/src/main/java/me/makeachoice/myappportfolio/fragment/SimpleButtonListFragment.java
+++ app/src/main/java/me/makeachoice/myappportfolio/fragment/SimpleButtonListFragment.java
@@ -13,6 +13,20 @@ import me.makeachoice.myappportfolio.R;
  */
 public class SimpleButtonListFragment extends Fragment {
 
+    /*
+        Fragment subclasses require an empty default constructor. If you don't provide one but
+        specify a non-empty constructor, Lint will give you an error.
+        Android may destroy and later re-create an activity and all its associated fragments when
+        the app goes into the background. When the activity comes back, its FragmentManager starts
+        re-creating fragments by using the empty default constructor. If it cannot find one, you
+        get an exception
+     */
+    public static SimpleButtonListFragment newInstance(){
+        return new SimpleButtonListFragment();
+    }
+
+    public SimpleButtonListFragment(){}
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
