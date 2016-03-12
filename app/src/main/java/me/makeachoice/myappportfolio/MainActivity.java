4aeb29a37e480de5091431f9bdebceece7636b9a
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-11 21:45:54 -0800

    Add fragment dynamically on MainActivity for vertical orientation

--- app/src/main/java/me/makeachoice/myappportfolio/MainActivity.java
+++ app/src/main/java/me/makeachoice/myappportfolio/MainActivity.java
@@ -1,21 +1,76 @@
 package me.makeachoice.myappportfolio;
 
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import me.makeachoice.myappportfolio.fragment.SimpleGridFragment;
+import me.makeachoice.myappportfolio.fragment.SimpleListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    OrientationEventListener myOrientationEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Note setContent must happen before toolbar
         setContentView(R.layout.activity_main);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            SimpleListFragment listFragment = new SimpleListFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            listFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, listFragment).commit();
+        }
+
+
+        myOrientationEventListener
+                = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL){
+
+            @Override
+            public void onOrientationChanged(int arg0) {
+
+                Toast.makeText(getApplicationContext(), "Orientation: " + String.valueOf(arg0), Toast.LENGTH_LONG).show();
+            }};
+
+        if (myOrientationEventListener.canDetectOrientation()){
+            Toast.makeText(this, "Can DetectOrientation", Toast.LENGTH_SHORT).show();
+            myOrientationEventListener.enable();
+        }
+        else{
+            Toast.makeText(this, "Can't DetectOrientation", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -43,11 +98,23 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        String strAction;
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_bar_main_simple) {
+            strAction = this.getString(R.string.action_bar_main_simple);
+        }
+        else if (id == R.id.action_bar_main_advance) {
+            strAction = this.getString(R.string.action_bar_main_advance);
+        }
+        else if (id == R.id.action_bar_main_grid) {
+            strAction = this.getString(R.string.action_bar_main_grid);
+        }
+        else {
+            strAction = "no action selected";
         }
 
+        Toast toast = Toast.makeText(this, strAction, Toast.LENGTH_SHORT);
+        toast.show();
         return super.onOptionsItemSelected(item);
     }
 }
