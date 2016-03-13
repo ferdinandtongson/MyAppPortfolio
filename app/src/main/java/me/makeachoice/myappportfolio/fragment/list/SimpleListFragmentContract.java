30367c30880172a788e7744287b956c73318d3ea
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-13 16:13:08 -0700

    Add SimpleListFragmentContract class to git

---
+++ app/src/main/java/me/makeachoice/myappportfolio/fragment/list/SimpleListFragmentContract.java
@@ -0,0 +1,13 @@
+package me.makeachoice.myappportfolio.fragment.list;
+
+/**
+ * SimpleListFragmentContract - static values used across all SimpleListFragmentX classes
+ */
+public class SimpleListFragmentContract {
+    /* To prevent someone from accidentally instantiating the contract class */
+    public SimpleListFragmentContract () {}
+
+    public static abstract class SimpleListFragmentValue {
+        public static final String BUNDLE_KEY_LIST = "List";
+    }
+}
