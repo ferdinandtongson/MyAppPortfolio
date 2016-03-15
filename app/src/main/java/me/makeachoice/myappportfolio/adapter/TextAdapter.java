0d4a2c79b0a7361d58f741c09e3e011742c2c0b9
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-15 13:07:51 -0700

    Extend BaseAdapter in TextAdapter class

--- app/src/main/java/me/makeachoice/myappportfolio/adapter/TextAdapter.java
+++ app/src/main/java/me/makeachoice/myappportfolio/adapter/TextAdapter.java
@@ -1,39 +1,52 @@
 package me.makeachoice.myappportfolio.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.List;
+
 import me.makeachoice.myappportfolio.R;
+import me.makeachoice.myappportfolio.adapter.item.OnlyText;
 
 /**
  * Created by Usuario on 3/15/2016.
  */
-public class TextAdapter {
+public class TextAdapter extends BaseAdapter {
     private Context mContext;
+    List<OnlyText> mRowItem;
 
-    public TextAdapter(Context c) {
+    public TextAdapter(Context c, List<OnlyText> rowItem) {
+        Log.d("SimpleListFragment", "TextAdapter: " + c.toString());
         mContext = c;
+        mRowItem = rowItem;
     }
 
+    @Override
     public int getCount() {
-        return mThumbIds.length;
+        return mRowItem.size();
     }
 
+    @Override
     public Object getItem(int position) {
-        return null;
+        return mRowItem.get(position);
     }
 
+    @Override
     public long getItemId(int position) {
-        return 0;
+        return mRowItem.indexOf(getItem(position));
     }
 
     // create a new ImageView for each item referenced by the Adapter
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Log.d("SimpleListFragment", "TextAdapter.getView");
         TextView textView;
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
@@ -45,17 +58,11 @@ public class TextAdapter {
             textView = (TextView) convertView;
         }
 
-        textView.setText(mThumbIds[position]);
+
+        OnlyText row_pos = mRowItem.get(position);
+        textView.setText(row_pos.getTitle());
         return textView;
     }
 
-    // references to our images
-    private String[] mThumbIds = {
-            "Test1", "Test2",
-            "Test3", "Test4",
-            "Test5", "Test6",
-            "Test7", "Test8",
-            "Test9", "Test0"
-    };
 
 }
