db9c40174319dd8084f9b2d6db119a0a4edb31b4
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-15 18:43:34 -0700

    Add  class variables to MyBaseAdapter

---
+++ app/src/main/java/me/makeachoice/myappportfolio/adapter/MyBaseAdapter.java
@@ -0,0 +1,19 @@
+package me.makeachoice.myappportfolio.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import me.makeachoice.myappportfolio.adapter.item.MyBaseRowItem;
+
+
+/**
+ * MyBaseAdapter - abstract class to act as my Base class for all my custom Adapters
+ */
+public abstract class MyBaseAdapter extends BaseAdapter{
+    Context mContext;
+    LayoutInflater mInflator;
+    List<MyBaseRowItem> mRowItem;
+}
