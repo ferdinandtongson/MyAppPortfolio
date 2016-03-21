ec36ac6a58a2d5a24b3041617dcfe8b0e35e89a4
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-21 15:00:48 -0700

    Add TitleIconItem class to git

---
+++ app/src/main/java/me/makeachoice/myappportfolio/adapter/item/TitleIconItem.java
@@ -0,0 +1,63 @@
+package me.makeachoice.myappportfolio.adapter.item;
+
+/**
+ * TitleIconItem extends TitleItem class and is used by a BaseAdapter to populate a ListView.
+ * This class is associated with item_titleicon.xml layout resource file and encapsulates the View
+ * created by item_titleicon.xml.
+ *
+ * TitleIconItem will be populated in the Presenter taking data from the Model.
+ *
+ * In your Presenter, adding data to your adapter before calling setListAdapter():
+ *      ArrayList<TitleIconItem> rowItems = new ArrayList<TitleIconItem>();
+ *      for(int i = 0; i < count; i++){
+ *          MyBaseRowItem item = new TitleIconItem(title_array[i]);
+ *          rowItems.add(item);
+ *      }
+ *
+ *      [X]Adapter adapter = new [X]Adapter(context, rowItems);
+ *      setListAdapter(adapter);
+ *
+ *  In getView(...) in your Adapter, get view and corresponding data for view:
+ *      TextView txtTitle = (TextView)itemView.findViewById(R.id.item_title_txtTitle)
+ *
+ *      TitleItem item = mRowItem.get(position);
+ *      txtTitle.setText(item.getTitle());
+ *      ....
+ *
+ */
+public class TitleIconItem extends TitleItem{
+
+    // mIconId - holds the resource Id number for the icon
+    private int mIconId;
+
+/**************************************************************************************************/
+/**
+ * TitleIconItem(String) - constructor, accepts string value for title and int value for icon id.
+ * Inherits from TitleItem:
+ *      mTitle - holds the string value of the title
+ *      String getTitle() - getter method for title
+ *      void setTitle(String title) - setter method for title
+ */
+    public TitleIconItem(){}
+
+    public TitleIconItem(String title, int iconId){
+        mTitle = title;
+        mIconId = iconId;
+    }
+
+/**************************************************************************************************/
+    /**
+     * Getter and Setter for text value Title
+     * getIcon() returns the Icon; int
+     * setIcon(String) sets the int value of the icon id
+     */
+    public int getIcon(){
+        return mIconId;
+    }
+
+    public void setIcon(int iconId){
+        mIconId = iconId;
+    }
+/**************************************************************************************************/
+
+}
