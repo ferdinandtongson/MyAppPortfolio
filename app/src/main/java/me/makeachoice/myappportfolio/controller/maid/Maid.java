3568d65de5fc909309cbe2c3a734dfd55b5f1c57
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-22 12:23:25 -0700

    Add a base class stub for Maid classes, Maid class

---
+++ app/src/main/java/me/makeachoice/myappportfolio/controller/maid/Maid.java
@@ -0,0 +1,10 @@
+package me.makeachoice.myappportfolio.controller.maid;
+
+import me.makeachoice.myappportfolio.controller.Boss;
+
+/**
+ * Created by Usuario on 3/22/2016.
+ */
+public abstract class Maid{
+    Boss mBoss;
+}
