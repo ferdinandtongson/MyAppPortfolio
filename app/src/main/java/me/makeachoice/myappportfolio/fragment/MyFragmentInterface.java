e568e08ac364bedf8e0be9fe754df54e4143689a
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-23 12:59:38 -0700

    Add MyFragmentInterface to git

---
+++ app/src/main/java/me/makeachoice/myappportfolio/fragment/MyFragmentInterface.java
@@ -0,0 +1,11 @@
+package me.makeachoice.myappportfolio.fragment;
+
+import me.makeachoice.myappportfolio.controller.maid.Maid;
+
+/**
+ * Created by Usuario on 3/23/2016.
+ */
+public interface MyFragmentInterface {
+
+    void setLayout(int id);
+}
