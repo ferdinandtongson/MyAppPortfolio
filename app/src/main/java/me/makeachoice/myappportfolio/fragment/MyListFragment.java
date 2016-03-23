87090b769bc923a7db4c3651effbe692e682d4c5
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-23 13:05:00 -0700

    Move Bridge interface fom SimpleListFragment to MyListFragment

--- app/src/main/java/me/makeachoice/myappportfolio/fragment/MyListFragment.java
+++ app/src/main/java/me/makeachoice/myappportfolio/fragment/MyListFragment.java
@@ -1,6 +1,7 @@
 package me.makeachoice.myappportfolio.fragment;
 
 import android.support.v4.app.ListFragment;
+import android.widget.ListAdapter;
 
 import me.makeachoice.myappportfolio.controller.maid.Maid;
 
@@ -8,7 +9,16 @@ import me.makeachoice.myappportfolio.controller.maid.Maid;
  * Created by Usuario on 3/23/2016.
  */
 public abstract class MyListFragment extends ListFragment implements MyFragmentInterface{
-    int mLayoutId;
-    Maid mMaid;
+    protected int mLayoutId;
+    protected Maid mMaid;
     abstract public void setLayout(int id);
+
+    //Upkeeping Maid class must implement this interface
+    public interface Bridge{
+        String KEY_LAYOUT = "Layout";
+
+        void onItemClick(int position);
+        ListAdapter getListAdapter();
+    }
+
 }
