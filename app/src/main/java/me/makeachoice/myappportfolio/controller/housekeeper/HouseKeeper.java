10d69962089284bd50279ef54beda9f893a3d724
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-25 17:37:18 -0700

    Add HouseKeeper abstract class to git

---
+++ app/src/main/java/me/makeachoice/myappportfolio/controller/housekeeper/HouseKeeper.java
@@ -0,0 +1,10 @@
+package me.makeachoice.myappportfolio.controller.housekeeper;
+
+import me.makeachoice.myappportfolio.controller.Boss;
+
+/**
+ * Created by Usuario on 3/25/2016.
+ */
+public abstract class HouseKeeper {
+    Boss mBoss;
+}
