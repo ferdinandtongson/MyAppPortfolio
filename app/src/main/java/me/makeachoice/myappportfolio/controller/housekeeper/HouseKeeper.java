bbcc0808b437470264acbe7f56894278975f174c
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2016-03-31 10:18:44 -0700

    Add variable into base HouseKeeper class

--- app/src/main/java/me/makeachoice/myappportfolio/controller/housekeeper/HouseKeeper.java
+++ app/src/main/java/me/makeachoice/myappportfolio/controller/housekeeper/HouseKeeper.java
@@ -1,10 +1,26 @@
 package me.makeachoice.myappportfolio.controller.housekeeper;
 
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+
+import java.util.HashMap;
+
 import me.makeachoice.myappportfolio.controller.Boss;
 
 /**
  * Created by Usuario on 3/25/2016.
  */
 public abstract class HouseKeeper {
+    //mBoss - application context object
     Boss mBoss;
+
+    //mActivityContext - context object that follows the Activity lifecycle
+    Context mActivityContext;
+
+    //mFragmentManager - manages fragments for the HouseKeeper
+    FragmentManager mFragmentManager;
+
+    //mMapFragmentType - hashmap used to hold different fragments types and their type variations
+    HashMap<String, Integer> mMapFragmentType;
+
 }
